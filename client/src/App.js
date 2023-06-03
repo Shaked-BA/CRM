0d3e4ae984783444dcc51610c20e631013f516bf
Shaked-BA <shaked.ba3@gmail.com>
2023-06-03 18:29:40 +0300

    added routes in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import './App.css';
+import NavBar from './components/NavBar';
+import Dashboard from './pages/Dashboard';
+import Ticket from './pages/Ticket';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/ticket" element={<Ticket />} />
+          <Route path="/ticket/:id" element={<Ticket editMode={true} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/components/TicketCard.js
@@ -0,0 +1,10 @@
+
+function TicketCard() {
+
+    return (
+      <div className="Ticket-Card">TicketCard</div>
+    );
+  }
+  
+  export default TicketCard;
+  
